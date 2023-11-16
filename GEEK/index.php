c1432d5d4e1582294e9689e67d39dfb453522ded
Wojtek08 <woj-maz1401@o2.pl>
2023-11-17 00:53:05 +0100

    1

--- GEEK/index.php
+++ GEEK/index.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Projekt GEEK</title>
     <link rel="stylesheet" href="style.css">
 </head>
 
@@ -18,12 +19,13 @@
                     <li>Cos2</li>
                 </ul>
                 <div id="chapter">
-                    <h1>Chapter 5 - Próba Lądowania</h1>
+                    <h1>Chapter 5</h1>
+                    <h1>Próba Lądowania</h1>
                 </div>
                 <ul id="right-header-container">
-                    <li>Ludnosc</li>
-                    <li>Paliwo</li>
-                    <li>Waluta</li>
+                    <li><i class="fa-solid fa-person"></i></li>
+                    <li><i class="fa-solid fa-gas-pump"></i></li>
+                    <li><i class="fa-solid fa-sack-dollar"></i></li>
                 </ul>
             </nav>
         </section>
@@ -34,7 +36,7 @@
                 <h1>Start</h1>
             </div>
             <div id=>
-                <h1>Jakieś Coś</h1>
+                <h1>Ustawienia</h1>
             </div>
         </section>
     </main>
