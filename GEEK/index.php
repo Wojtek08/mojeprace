19fa154ab613d958759919a097affd6087ee58d5
Wojtek08 <woj-maz1401@o2.pl>
2023-11-17 00:23:54 +0100

    first commit

---
+++ GEEK/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <section id="header-section">
+            <nav>
+                <ul id="left-header-container">
+                    <li>Cos</li>
+                    <li>Cos1</li>
+                    <li>Cos2</li>
+                </ul>
+                <div id="chapter">
+                    <h1>Chapter 5 - Próba Lądowania</h1>
+                </div>
+                <ul id="right-header-container">
+                    <li>Ludnosc</li>
+                    <li>Paliwo</li>
+                    <li>Waluta</li>
+                </ul>
+            </nav>
+        </section>
+    </header>
+    <main>
+        <section id="main-section">
+            <div id="start">
+                <h1>Start</h1>
+            </div>
+            <div id=>
+                <h1>Jakieś Coś</h1>
+            </div>
+        </section>
+    </main>
+</body>
+
+</html>
